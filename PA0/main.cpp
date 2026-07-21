c12d0c8f0cf378b160b41a491d0f050b211566a8
Xan-WH1Te <hjw1656050422@google.com>
2026-07-21 15:31:55 +0800

    feat: complete PA0 - Eigen transformation

---
+++ PA0/main.cpp
@@ -0,0 +1,25 @@
+#include <eigen3/Eigen/Core>
+#include <cmath>
+#include <iostream>
+int main() {
+    Eigen::Vector3f p(2.0f, 1.0f, 1.0f);
+
+    // 45 degrees to radians
+    float angle = 45.0f / 180.0f * M_PI;
+
+    Eigen::Matrix3f rotate;
+    rotate << std::cos(angle), -std::sin(angle), 0,
+              std::sin(angle),  std::cos(angle), 0,
+               0,                0,               1;
+
+    Eigen::Matrix3f translate;
+    translate << 1, 0, 1,
+                 0, 1, 2,
+                 0, 0, 1;
+
+    Eigen::Vector3f result = translate * rotate * p;
+
+    std::cout << result << std::endl;
+    return 0;
+}
+
